223e369b0406e73c9e8500c3dc44cc7d69b983c8
GaspardRuan <1039553124@qq.com>
2024-03-28 23:40:52 +0800

    fix: make clearCoverageAnalysis action

--- src/renderer/state.ts
+++ src/renderer/state.ts
@@ -72,6 +72,7 @@ export class AppState {
       cfgButtonEnabled: computed,
       clearSlice: action,
       clearDefUse: action,
+      clearCoverageAnalysis: action,
       controlFlowActive: computed,
       dcPathExtractActive: computed,
       defUseActive: computed,
@@ -116,6 +117,7 @@ export class AppState {
     this.setupDefUse = this.setupDefUse.bind(this);
     this.analyzeCoverage = this.analyzeCoverage.bind(this);
     this.clearDefUse = this.clearDefUse.bind(this);
+    this.clearCoverageAnalysis = this.clearCoverageAnalysis.bind(this);
 
     // Setup auto-runs
     autorun(() => this.save(GlobalSetting.theme, this.theme));
