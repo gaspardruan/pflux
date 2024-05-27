a98674693a47f0655b49c8a670c66c2dd10c990c
GaspardRuan <1039553124@qq.com>
2024-05-27 15:12:32 +0800

    chore: delete tests

--- tests/utils/editor-utils-spec.ts
+++
@@ -1,9 +0,0 @@
-import { getEmptyContent } from '../../src/utils/editor-utils';
-
-describe('editor-utils', () => {
-  describe('getEmptyContent', () => {
-    it('returns empty comments for target filename', () => {
-      expect(getEmptyContent('main.py')).toBe('# main.py\n');
-    });
-  });
-});
